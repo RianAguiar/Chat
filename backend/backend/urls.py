3fec938c2aa9092c9cf707c03701982539dba21c
Your Name <aguiarsantosrian@gmail.com>
2026-05-22 14:20:20 -0300

    front

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
+    TokenObtainPairView, # gera um token de autenticação quando faz login
+    TokenRefreshView, # gera um novo token de acesso quando o antecessor é expirado
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view()),
-    path('api/token/refresh', TokenRefreshView.as_view()),
+    path('api/token/', TokenObtainPairView.as_view()), # rota de login
+    path('api/token/refresh', TokenRefreshView.as_view()), # rota de renovação
 ]
