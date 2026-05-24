037cb16493a908231c0423ce465614b3238411ee
Your Name <aguiarsantosrian@gmail.com>
2026-05-24 17:08:55 -0300

    commenting

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,13 +1,19 @@
-
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.routing import ProtocolTypeRouter # decide qual viwe ou consumer usar dependendo do tipo de requisição sendo http ou websocket
+from channels.routing import URLRouter # urlrouter faz a mesma função do "urls.py" porém para websocket
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')# define qual arquivo o de configuração o django vai usar
 
-application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+application = ProtocolTypeRouter({ #chama o protocolotyperouter, se a requisição for http ele envia pra as views
+    'http': get_asgi_application(), #  se for websocket envia a requisição para algum consumer
     'websocket': AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns)),
 })
+
+'''
+servidores wsgi (padrao django) processa cada task por vez, já os servidores assincronos(asgi) processa varias requisições por vez
+tambem o asgi tem suporte para websocket(o wsgi n tem suporte à websocket)
+esse arquivo é onde configura o django para suportar o padrao asgi
+'''
