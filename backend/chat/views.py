f2e9f0f653acb6d7a6243d6836217629fdf953e3
Your Name <aguiarsantosrian@gmail.com>
2026-06-29 23:42:37 -0300

    views prontas (eu acho KKKKKKKKKKKKKKKKKK)

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,9 +1,30 @@
-from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Mensagem, Sala
 from .serializers import MensagemSerializer, SalaSerializer
 
-class GetMensagens(viewsets.ModelViewSet):
-    queryset = Mensagem.objects.all()
-    serializer_class = MensagemSerializer
 
-class 
+#Buscar sala, ver se existe, se existir envia o usuario para essa sala e resgata as mensagens enteriores,
+# se n existir cria automaticamente a sala e envia o usuario para ela
+class SalaAPIView(APIView):
+    def get(self, nome): 
+        sala = Sala.objects.get_or_create(nome=nome)
+        serializer = SalaSerializer(sala)
+        return Response(serializer.data)
+
+class MensagensAPIView(APIView):
+    def get(self):
+        mensagens = Mensagem.objects.filter(Sala=Sala)
+        serializer = MensagemSerializer(mensagens, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request, id):
+        sala = Sala.objects.get(id=id)
+        mensagem = Mensagem.objects.create(
+            sala = sala,
+            username = request.data.get('username'),
+            conteudo = request.data.get('conteudo'),
+            enviado_as = request.data('enviado_as'),
+            )
+        serializer = MensagemSerializer(mensagem)
+        return Response(serializer.data, status=201)
